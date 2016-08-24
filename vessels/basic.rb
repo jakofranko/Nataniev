00f68f871e62839623ab53641d7fe717ea94ef80
Devine Lu Linvega <aliceffekt@gmail.com>
2016-08-23 20:23:23 -0700

    Basic vessel methods

--- vessels/basic.rb
+++ vessels/basic.rb
@@ -5,6 +5,8 @@ class Basic
 
 	include Vessel
 
+	# System
+
 	def connect p = nil
 
 		return "Connected"
@@ -12,9 +14,129 @@ class Basic
 	end
 
 	def look p = nil
-		
+
 		return "Looking.."
 
+	end
+
+	# Basic
+
+	def create p = nil
+
+		return "TODO"
+		
+	end
+
+	def become p = nil
+
+		return "TODO"
+		
+	end
+
+	def enter p = nil
+
+		return "TODO"
+		
+	end
+
+	def leave p = nil
+
+		return "TODO"
+		
+	end
+
+	# Narative
+
+	def note p = nil
+
+		return "TODO"
+		
+	end
+
+	def rename p = nil
+
+		return "TODO"
+		
+	end
+
+	# Warp
+
+	def warp p = nil
+
+		return "TODO"
+		
+	end
+
+	def random p = nil
+
+		return "TODO"
+		
+	end
+
+	# Inventory
+
+	def inventory p = nil
+
+		return "TODO"
+		
+	end
+
+	def take p = nil
+
+		return "TODO"
+		
+	end
+
+	def drop p = nil
+
+		return "TODO"
+		
+	end
+
+	# Programming
+
+	def program p = nil
+
+		return "TODO"
+		
+	end
+
+	def use p = nil
+
+		return "TODO"
+		
+	end
+
+	def call p = nil
+
+		return "TODO"
+		
+	end
+
+	# Security
+
+	def lock p = nil
+
+		return "TODO"
+		
+	end
+
+	def unlock p = nil
+
+		return "TODO"
+		
+	end
+
+	def show p = nil
+
+		return "TODO"
+		
+	end
+
+	def hide p = nil
+
+		return "TODO"
+		
 	end
 	
 end
