a0cbe51b9b0ae89de680a6fd43726904a2877528
Devine Lu Linvega <aliceffekt@gmail.com>
2016-09-16 10:02:52 -0700

    Implemented examine

--- actions/examine.rb
+++ actions/examine.rb
@@ -3,10 +3,37 @@
 
 module ActionExamine
 
+  include Action
+
   def examine q = nil
 
-    return "! TODO"
+    v = find_visible_vessel(q) ; if !v then return v = @actor end
+
+    return sonar(v)
     
+  end
+
+  private
+
+  def sonar v = nil
+
+    tries = 0
+    parent = v.id
+    while tries < 100
+      code = $nataniev.parade.to_a[parent]["CODE"]
+      if parent == code[5,5].to_i 
+        if tries == 0
+          return "! The #{v.name} is at the stem of #{$nataniev.make_vessel(parent).print} universe."
+        else
+          return "! The #{v.name} is #{tries} levels deep, within the #{$nataniev.make_vessel(parent).print} universe." 
+        end
+      end
+      parent = code[5,5].to_i
+      tries += 1
+    end
+
+    return "! The #{v.name}(##{v.id}), is within a circular paradox."
+
   end
 
 end
--- vessels/basic.rb
+++ vessels/basic.rb
@@ -34,8 +34,6 @@ class Basic
 
     include ActionInventory
 
-    include ActionSonar
-
   end
 
   class ParentActions
