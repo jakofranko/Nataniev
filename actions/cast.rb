4cb3f2d28b6624e80a20de765fa66da2e1c8934b
Devine Lu Linvega <aliceffekt@gmail.com>
2016-09-16 15:12:39 -0700

    Cast validation

--- actions/cast.rb
+++ actions/cast.rb
@@ -5,11 +5,14 @@ module ActionCast
 
   def cast q = nil
 
+  	instance_name = q.to_s.split(" ").last.to_s.upcase
+
   	if @target.owner != @actor.id then return error_owner(v.name) end
 
-  	if q.to_s.length != 5 then return "#{q.to_s.capitalize} is not a vessel type." end
+  	if instance_name.length != 5 then return "! #{instance_name.upcase} is not a vessel type." end
+	if !File.exist?("#{$nataniev_path}/vessels/#{instance_name}.rb") then return "! #{instance_name} is not a valid instance cast." end
 
-    return @target.set_instance(q.to_s) ? "! You casted #{@target.print} into a #{q} vessel." : "! The #{@target.name} is locked."
+    return @target.set_instance(instance_name) ? "! You casted #{@target.print} into a #{instance_name.upcase} vessel." : "! The #{@target.name} is locked."
     
   end
 
