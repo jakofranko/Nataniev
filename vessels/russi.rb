bec3ccd953168a2f05912d3e9e8a2158957b3471
Devine Lu Linvega <aliceffekt@gmail.com>
2016-10-08 02:41:41 -0700

    Improved the Russi vessel

--- vessels/russi.rb
+++ vessels/russi.rb
@@ -5,8 +5,20 @@ class Russi
 
   include Vessel
 
-  def dict ; return Di.new("russian.vocabulary").to_a end
-  def answer ; word = Di.new("russian.vocabulary").to_a.sample ; return "! The Russian word \"#{word['RUSSIAN']}\" means \"#{word['ENGLISH']}\"." end
+  class PassiveActions
+
+    include ActionCollection
+
+    def answer q = nil
+
+      word = Di.new("russian.vocabulary").to_a.sample
+      return "! The Russian word \"#{word['RUSSIAN']}\" means \"#{word['ENGLISH']}\"."
+
+    end
+
+  end
+
+  def passive_actions ; return PassiveActions.new(self,self) end
 
   # Actions
 
