5f54aaf518ab0490a0afb325bc742ccb400f13bc
root <aliceffekt@gmail.com>
2016-12-14 13:31:09 -0500

    Improved setup.

--- nataniev.server.rb
+++ nataniev.server.rb
@@ -5,7 +5,10 @@ require 'sinatra'
 
 require_relative "system/nataniev.rb"
 
+set :port, 8888
+
 get '/:task' do
   $nataniev = Nataniev.new
+  puts request.base_url
   "#{$nataniev.answer("#{ARGV.first} serve "+params[:task])}"
 end
