636b90d660d51f84860e2da30b3dddd6720faf74
Devine Lu Linvega <aliceffekt@gmail.com>
2017-01-29 21:51:14 -0700

    Added splash

--- nataniev.server.rb
+++ nataniev.server.rb
@@ -12,7 +12,8 @@ get '/' do
   puts request.base_url
   
   v = ARGV.first ? ARGV.first : "ghost"
-  if request.base_url.include? "xxiivv" then v = "oscean" end
+  if request.base_url.include? "xxiivv" then v = "landing" end
+  if request.base_url.include? "wiki" then v = "oscean" end
   if request.base_url.include? "grimgrains" then v = "grimgrains" end
   if request.base_url.include? "100r" then v = "hundredrabbits" end
   if request.base_url.include? "paradise" then v = "paradise" end
@@ -28,7 +29,8 @@ get '/:task' do
   puts request.base_url
   
   v = ARGV.first ? ARGV.first : "ghost"
-  if request.base_url.include? "xxiivv" then v = "oscean" end
+  if request.base_url.include? "xxiivv" then v = "landing" end
+  if request.base_url.include? "wiki" then v = "oscean" end
   if request.base_url.include? "grimgrains" then v = "grimgrains" end
   if request.base_url.include? "100r" then v = "hundredrabbits" end
   if request.base_url.include? "paradise" then v = "paradise" end
