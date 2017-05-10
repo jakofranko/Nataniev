8dcb8cd44729a316da5342e710c9f98c32c68064
Devine Lu Linvega <aliceffekt@gmail.com>
2017-05-09 21:06:51 -1000

    Added tables

--- core/corpse/core.http/array.rb
+++ core/corpse/core.http/array.rb
@@ -6,14 +6,16 @@ class Array
   def runes_collection
 
     return {
-      "&" => {"tag" => "p"},
-      "-" => {"tag" => "list", "stash" => true},
-      "#" => {"tag" => "code", "stash" => true},
-      "?" => {"tag" => "nt"},
-      "*" => {"tag" => "h2"},
-      "=" => {"tag" => "h3"},
-      "+" => {"tag" => "hs"},
-      ">" => {"tag" => ""}
+      "&" => {:tag => "p"},
+      "-" => {:tag => "list", :stash => true},
+      "#" => {:tag => "code", :stash => true},
+      "?" => {:tag => "nt"},
+      "*" => {:tag => "h2"},
+      "=" => {:tag => "h3"},
+      "+" => {:tag => "hs"},
+      "|" => {:tag => "tr", :sub => "td", :rep => true},
+      "»" => {:tag => "tr", :sub => "th", :rep => true},
+      ">" => {:tag => ""}
     }
     
   end
@@ -29,19 +31,26 @@ class Array
     self.each do |line|
       rune = line[0,1]
       text = line.sub(rune,"").strip
-      tag  = collection[rune] ? collection[rune]['tag'] : "unknown"
+      tag  = collection[rune] ? collection[rune][:tag] : "unknown"
+      sub_tag  = collection[rune] ? collection[rune][:sub] : "unknown"
 
       if rune == "%" then html += Media.new(cat,text.split(" ").first,text.split(" ")[1,3].join(" ")).to_s ; next end
       if rune == "$" then html += (n = Nataniev.new ; n.answer(text) ) ; next end
 
       if stash != "" && rune != prev
-        prev_tag = collection[prev]["tag"]
+        prev_tag = collection[prev][:tag]
         html += "<#{prev_tag}>#{stash}</#{prev_tag}>"
         stash = ""
       end
 
-      if collection[rune] && collection[rune]["stash"]
+      if collection[rune] && collection[rune][:stash]
         stash += "#{text}<br />"
+      elsif collection[rune] && collection[rune][:rep]
+        rep = ""
+        text.split("|").each do |seg|
+          html += "<#{sub_tag}>#{seg}</#{sub_tag}>"
+        end
+        html += "<#{tag}>#{rep}</#{tag}>"
       elsif tag == ""
         html += "#{text}"
       else
