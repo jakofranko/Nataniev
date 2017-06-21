e026db04af094c5c60e8d641f497c889dcdd0038
Devine Lu Linvega <aliceffekt@gmail.com>
2017-06-21 12:56:47 -1000

    test

--- nataniev.server.rb
+++ nataniev.server.rb
@@ -2,29 +2,18 @@
 # encoding: utf-8
 
 require 'sinatra'
-require 'sinatra/cross_origin'
 
 require_relative "system/nataniev.rb"
+
+set :port, 8888
 
 configure do
   enable :cross_origin
 end
 
-set :port, 8888
-
-set :allow_origin, :any
-set :allow_methods, [:get, :post, :options]
-set :allow_credentials, true
-set :max_age, "1728000"
-set :expose_headers, ['Content-Type']
-
-options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-  200
-end
-
 get '/' do
+
+  headers( "Access-Control-Allow-Origin" => "*" )
   $nataniev = Nataniev.new
   
   v = ARGV.first ? ARGV.first : "ghost"
