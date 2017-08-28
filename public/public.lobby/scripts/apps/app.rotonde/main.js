589fd7fec66a0555bf02f45a96d4ac73fea13cc3
Devine Lu Linvega <aliceffekt@gmail.com>
2017-08-29 09:43:02 +1300

    Improved Rotonde client

--- public/public.lobby/scripts/apps/app.rotonde/main.js
+++ public/public.lobby/scripts/apps/app.rotonde/main.js
@@ -3,8 +3,9 @@ function Rotonde()
   App.call(this);
 
   this.name = "rotonde";
-  this.size = {width:600,height:300};
-  this.origin = {x:30,y:30};
+  this.window.size = {width:600,height:300};
+  this.window.pos = {x:30,y:30};
+  this.window.theme = "blanc";
 
   this.methods.update = {name:"update"};
   this.methods.portal = {name:"portal"};
@@ -13,44 +14,34 @@ function Rotonde()
   this.methods.refresh = {name:"refresh"};
   this.rotonde = null;
 
-  this.includes = ["instance"];
+  this.portals = [];
 
-  this.on_launch = function()
+  this.setup.includes = ["portal"];
+
+  this.setup.start = function()
   {
-    this.wrapper_el.innerHTML = "Loading feed..";
+    // Select portals
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.v-os.ca"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde-joshavanier.hashbase.io"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.v-os.ca"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.monochromatic.co"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.anxl.faith"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.electricgecko.de"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.attilam.com"));
+    this.app.portals.push(new Rotonde_Portal("http://brennan-ltkmn.hashbase.io"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde-ciel.hashbase.io"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.cblgh.org/network.json"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde.neufv.website"));
+    this.app.portals.push(new Rotonde_Portal("http://rotonde-somnius.hashbase.io"));
 
-    this.rotonde = JSON.parse('{"profile":{"name":"Devine Lu Linvega","location":"Huahine","avatar":"http://wiki.xxiivv.com/public.oscean/media/brand/logo.devine.lu.linvega.png","color":"#72dec2"},"feed":[{"time":"1498017600","text":"Started the Rotonde experiment.","url":"http://wiki.xxiivv.com/Rotonde}"},{"time":"1497931200","text":"Started building an IDE to build Nataniev within itself.","url":"http://wiki.xxiivv.com/Lobby}"},{"time":"1497844800","url":"http://wiki.xxiivv.com/Lobby}"},{"time":"1497758400","url":"http://wiki.xxiivv.com/Lobby}"},{"time":"1497672000","text":"Began created a frontend to the Nataniev server.","url":"http://wiki.xxiivv.com/Lobby}"},{"time":"1497585600","text":"Update the Ronin logo to a more simple design.","url":"http://wiki.xxiivv.com/Ronin}"},{"time":"1497499200","url":"http://wiki.xxiivv.com/Marabu}"},{"time":"1497412800","url":"http://wiki.xxiivv.com/Oquonie}"},{"time":"1497326400","url":"http://wiki.xxiivv.com/Marabu}"},{"time":"1497240000","url":"http://wiki.xxiivv.com/Marabu}"},{"time":"1497153600","url":"http://wiki.xxiivv.com/Marabu}"},{"time":"1497067200","media":"http://wiki.xxiivv.com/public.oscean/media/diary/194.jpg","url":"http://wiki.xxiivv.com/Marabu}"},{"time":"1496980800","url":"http://wiki.xxiivv.com/Marabu}"},{"time":"1496894400","url":"http://wiki.xxiivv.com/Oquonie}"},{"time":"1496808000","url":"http://wiki.xxiivv.com/Horaire}"},{"time":"1496721600","url":"http://wiki.xxiivv.com/Horaire}"},{"time":"1496635200","url":"http://wiki.xxiivv.com/Hundred rabbits}"},{"time":"1496548800","url":"http://wiki.xxiivv.com/Hundred rabbits}"},{"time":"1496462400","url":"http://wiki.xxiivv.com/Horaire}"},{"time":"1496376000","url":"http://wiki.xxiivv.com/Known magye}"},{"time":"1496289600","url":"http://wiki.xxiivv.com/Aliceffekt}"},{"time":"1496203200","media":"http://wiki.xxiivv.com/public.oscean/media/diary/10.jpg","url":"http://wiki.xxiivv.com/Collected works}"},{"time":"1496116800","media":"http://wiki.xxiivv.com/public.oscean/media/diary/179.jpg","url":"http://wiki.xxiivv.com/Children of bramble}"},{"time":"1496030400","url":"http://wiki.xxiivv.com/Dinaisth}"},{"time":"1495944000","url":"http://wiki.xxiivv.com/Dei dain}"},{"time":"1495857600","url":"http://wiki.xxiivv.com/Ronin}"},{"time":"1495771200","url":"http://wiki.xxiivv.com/Ronin}"},{"time":"1495684800","url":"http://wiki.xxiivv.com/Ronin}"},{"time":"1495598400","url":"http://wiki.xxiivv.com/Ronin}"},{"time":"1495512000","url":"http://wiki.xxiivv.com/Ronin}"},{"time":"1495425600","text":"Updated {{Ronin}} with a major design overhaul and various functionality improvements. It includes the ability to save & load .rin files. I have also updated the logo to fit within the {{Gamme}} ecosystem.","media":"http://wiki.xxiivv.com/public.oscean/media/diary/168.jpg","url":"http://wiki.xxiivv.com/Ronin}"}],"portal":["rotonde.monochromatic.co","rotonde.cblgh.org","rotonde.anxl.faith","johnakers.network/rotonde.json"]}');
-    // this.get_url("http://rotonde.xxiivv.com");
-
-    this.create_feed();
+    update();
   }
 
-  this.get_url_callback = function(url,r)
+  function update()
   {
-    console.log(url);
-    this.rotonde = r;
-  }
-
-  this.create_feed = function()
-  {
-    this.portals = [];
-
-    this.portals.push(new Rotonde_Instance("johnakers.network/rotonde.json"));
-    this.portals.push(new Rotonde_Instance("rotonde.cblgh.org"));
-    this.portals.push(new Rotonde_Instance("rotonde.monochromatic.co"));
-    this.portals.push(new Rotonde_Instance("rotonde.brennan.pizza/feed.json"));
-    this.portals.push(new Rotonde_Instance("rotonde.anxl.faith"));
-    this.portals.push(new Rotonde_Instance("rotonde.electricgecko.de"));
-    this.portals.push(new Rotonde_Instance("rotonde.attilam.com"));
-    
-    // this.portals.push(new Rotonde_Instance("rotonde.xxiivv.com"));
-
-    this.wrapper_el.innerHTML = "";
-
-    for(portal_id in this.portals){
-      var portal = this.portals[portal_id];
-      portal.init();
-      this.wrapper_el.appendChild(portal.el);
+    for(portal_id in lobby.apps.rotonde.portals){
+      var portal = lobby.apps.rotonde.portals[portal_id];
+      portal.install(lobby.apps.rotonde.wrapper_el);
     }
   }
 }
