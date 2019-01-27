5af9c1cddad4cb51291c30358b9967f482b356a2
Jake Franklin <jacob.d.franklin@gmail.com>
2019-01-26 22:37:44 -0700

    a few improvements to the console

--- nataniev.console.rb
+++ nataniev.console.rb
@@ -7,22 +7,33 @@ class Nataniev
 
 end
 
-def oroborus
+def ouroborus
 
+    print $nataniev.console_bind != "" ? "<#{$nataniev.console_bind.colorize(2)}> " : "> "
     while a = gets.strip
 
-        if a == "exit"
-            exit 0
-            return false
+        if a == "" && $nataniev.console_bind == ""
+            puts "[no input]"
+        elsif a == "exit"
+            break
+        elsif a == "help"
+            help
         elsif a.split(" ").first == "bind"
             $nataniev.console_bind = a.sub("bind","").strip
-            print $nataniev.console_bind != "" ? "<#{$nataniev.console_bind.colorize(2)}> " : "> "
         else
             puts $nataniev.answer($nataniev.console_bind != "" ? "#{$nataniev.console_bind} #{a}" : a)
-            print $nataniev.console_bind != "" ? "<#{$nataniev.console_bind.colorize(2)}> " : "> "
         end
 
+        print $nataniev.console_bind != "" ? "<#{$nataniev.console_bind.colorize(2)}> " : "> "
+
     end
+
+end
+
+def help
+
+    puts "To quit the Nataniev console, type 'exit'"
+    puts "To bind vessels and their actions to the console, type bind <vessel> (<action>)"
 
 end
 
@@ -34,14 +45,13 @@ begin
 
     puts "\n"
     puts "  #{Desamber.new}"
-    puts "  Nataniev is listening..."
-    print "\n> "
+    puts "  Nataniev is listening...\n"
 
-    oroborus
+    ouroborus
 
 rescue Exception => e
-    puts e
-
+    puts "There was an exception..."
+    puts "#{$!}"
     puts "\n\n#{$!.to_s.gsub('`','').gsub('\'','').capitalize}\n\n"
     $@.each do |e|
         line = e.scan(/(?:\:)([\w\W]*?)(?=\:)/).first.first
@@ -51,5 +61,5 @@ rescue Exception => e
     end
     puts "\n"
 
-    oroborus
+    ouroborus
 end
