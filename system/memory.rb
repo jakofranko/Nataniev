d0fb427099f40fe31038ef2f9f2779e5425d9653
Jake Franklin <jacob.d.franklin@gmail.com>
2024-10-22 07:26:18 -0700

    linting pass on memory

--- system/memory.rb
+++ system/memory.rb
@@ -1,48 +1,46 @@
 #!/bin/env ruby
-# encoding: utf-8
+require 'fileutils'
 
-require "fileutils"
-
+# This module is for managing the Nataniev data format: memories
 module Memory
+
   extend Gem::Deprecate
 
-  attr_accessor :name
-  attr_accessor :key
-  attr_accessor :path
-  attr_accessor :render
-  attr_accessor :data_start
+  attr_accessor :name, :key, :path, :render, :data_start
 
-  @@note_char = "~"
-  @@key_row_char = "@"
+  @@note_char = '~'
+  @@key_row_char = '@'
 
-  def initialize name = nil, dir = "#{$nataniev.path}"
+  def initialize(name = nil, dir = $nataniev.path.to_s)
 
-    @name    = "#{name}".downcase.gsub(" ",".")
-    @path    = make_path(dir,ext)
+    @name    = name.to_s.downcase.gsub(' ', '.')
+    @path    = make_path(dir, ext)
     @key     = nil # Created as part of the render
     @data_start = nil # File line index where the front-matter ends and the real data begins
-    @render  = make_render(get_file)
+    @render = make_render(get_file)
 
     # Used to handle simultaneous writes
     @is_updating = [] # Add line indexes when updating, then remove when done
-    @is_appending = [] # Add lines to append here with format [memory_index, new_line] e.g., [[0, "new line 1"], [1, "new line 2"]]
+
+    # Add lines to append here with format [memory_index, new_line] e.g., [[0, "new line 1"], [1, "new line 2"]]
+    @is_appending = []
 
   end
 
   def length
 
-    return @render.length
+    @render.length
 
   end
 
-  def overwrite content
+  def overwrite(content)
 
     # Create temp file
     dirs, file_name = File.split("#{$nataniev.path}/memory/#{@name}.tmp")
 
-    if !File.exists? dirs then FileUtils.mkdir_p dirs end
+    FileUtils.mkdir_p dirs unless File.exist? dirs
 
-    out_file = File.new("#{dirs}/#{file_name}", "w")
+    out_file = File.new("#{dirs}/#{file_name}", 'w')
     out_file.puts(content)
     out_file.close
 
@@ -54,7 +52,7 @@ module Memory
   end
   deprecate :overwrite, :replace_file, 2024, 12
 
-  def overwrite_line id, line
+  def overwrite_line(id, line)
 
     lines = File.readlines(@path)
     lines[id] = line << $/
@@ -66,25 +64,24 @@ module Memory
 
   private
 
-  def make_path dir,ext
+  def make_path(dir, ext)
 
-    if File.exist?("#{dir}/memory/#{name}.#{ext}")
-      return "#{dir}/memory/#{name}.#{ext}"
-    end
+    return "#{dir}/memory/#{name}.#{ext}" if File.exist?("#{dir}/memory/#{name}.#{ext}")
+
     puts "Cannot locate #{dir}/memory/#{name}.#{ext}"
-    return nil
+    nil
 
   end
 
   def get_file
 
-    return File.read(@path, :encoding => 'UTF-8').split("\n")
+    File.read(@path, encoding: 'UTF-8').split("\n")
 
   end
 
-  def replace_line memory_index, new_line
+  def replace_line(memory_index, new_line)
 
-    if @data_start.nil? then
+    if @data_start.nil?
       raise "#{@name} Memory: Data has not been rendered yet, and cannont replace line with #{new_line}"
     end
 
@@ -97,14 +94,14 @@ module Memory
 
   end
 
-  def replace_file content
+  def replace_file(content)
 
     # Perform an atomic update by first creating a temp file
     dirs, file_name = File.split("#{$nataniev.path}/memory/#{@name}.tmp")
 
-    if !File.exists? dirs then FileUtils.mkdir_p dirs end
+    FileUtils.mkdir_p dirs unless File.exist? dirs
 
-    out_file = File.new("#{dirs}/#{file_name}", "w")
+    out_file = File.new("#{dirs}/#{file_name}", 'w')
     out_file.puts(content)
     out_file.close
 
@@ -113,23 +110,24 @@ module Memory
 
   end
 
-  def append_line line
+  def append_line(line)
 
     File.open(@path, 'a') do |f|
+
       f.puts line
+
     end
 
   end
 
 end
 
-#
-
+# The Memory_Array is a table-like data structure
 class Memory_Array
 
   include Memory
 
-  def initialize name = nil, dir = nil
+  def initialize(name = nil, dir = nil)
     super
 
     @a = []
@@ -141,59 +139,66 @@ class Memory_Array
 
   end
 
-  def ext ; return "ma" end
+  def ext
+    'ma'
+  end
 
   # Methods
 
-  def filter field, value, type
+  def filter(field, value, type)
 
-    if field == @last_field && last_value == @last_value && type == @last_filter_type then
-      return @last_filtered_a
-    end
+    return @last_filtered_a if field == @last_field && value == @last_value && type == @last_filter_type
 
     new_filtered_a = []
 
-    if @a.length && type == @last_type then
+    if !@a.empty? && type == @last_type
       @a.each do |obj|
-        if value != "*" && (!obj.has(field) || !obj[field].to_s.like(value)) then next end
+
+        next if value != '*' && (!obj.has(field) || !obj[field].to_s.like(value))
+
         new_filtered_a.push(obj)
+
       end
     else
       @render.each do |line|
+
         # TODO: I don't think this works, but it was here...might need to fix
-        if value != "*" && !line[field].to_s.like(value) then next end
+        next if value != '*' && !line[field].to_s.like(value)
+
         new_filtered_a.push(type ? Object.const_get(type.capitalize).new(line) : line)
+
       end
     end
-
 
     @last_field = field
     @last_value = value
     @last_filter_type = type
     @last_filtered_a = new_filtered_a
-    return new_filtered_a
+    new_filtered_a
 
   end
 
-  def to_a type = nil
+  def to_a(type = nil)
 
     # Return cached array to avoid regenerating
-    if @a.length && type == @last_a_type then return @a end
+    return @a if @a.length && !type.nil? && type == @last_a_type
 
     @a = []
     i = 0
     @render.each do |line|
+
       @a.push(type ? Object.const_get(type.capitalize).new(line, i) : line)
       i += 1
+
     end
 
     @last_a_type = type
-    return @a
+    @a
 
   end
 
-  def append line
-    
+  def append(line)
+
     append_line line
 
     parsed_line = parse_line(@key, line)
@@ -202,18 +207,16 @@ class Memory_Array
 
   end
 
-  def update memory_index, new_line
+  def update(memory_index, new_line)
 
     already_updating_line = @is_updating.include? memory_index
 
-    if already_updating_line then
-      return false
-    end
+    return false if already_updating_line
 
     @is_updating.append(memory_index)
 
     # TODO: Do this in a new thread
-    self.replace_line(memory_index, new_line)
+    replace_line(memory_index, new_line)
 
     parsed_line = parse_line(@key, new_line)
     @render[memory_index] = parsed_line
@@ -228,111 +231,117 @@ class Memory_Array
   # rendered row (a single item from @render),
   # and converts it to a string that can be used to update
   # the memory file associated with this memory.
-  def hash_to_line row
+  def hash_to_line(row)
 
-      puts "HASH ROW"
-      puts row.inspect
-      puts @key
+    puts 'HASH ROW'
+    puts row.inspect
+    puts @key
 
     line = ''
     @key.each do |key, positions|
-        open = positions[0]
-        length = positions[1]
-        val = row[key.to_sym]
 
-        if val then
-            puts "appending"
-            line << val.append(" ", length)
-        end
+      length = positions[1]
+      val = row[key.to_sym]
 
-        puts 'LINE'
-        puts line.inspect
+      if val
+        puts 'appending'
+        line << val.append(' ', length)
+      end
+
+      puts 'LINE'
+      puts line.inspect
+
     end
 
-    return line
+    line
 
   end
 
   private
 
-  def make_render file
+  def make_render(file)
 
     array = []
 
     i = 0
     file.each do |line|
-        if line == "" || line[0,1] == @@note_char then
-          i += 1
-          next
-        end
-        if line[0,1] == @@key_row_char
-            @key = make_key(line)
-            i += 1
-        elsif @key
-            if @data_start.nil? then @data_start = i end # set where data begins
-            array.push(parse_line(@key,line))
-        end
+
+      if line == '' || line[0, 1] == @@note_char
+        i += 1
+        next
+      end
+      if line[0, 1] == @@key_row_char
+        @key = make_key(line)
+        i += 1
+      elsif @key
+        if @data_start.nil? then @data_start = i end # set where data begins
+        array.push(parse_line(@key, line))
+      end
+
     end
 
-    return array
+    array
 
   end
 
-  def make_key key_line
+  def make_key(key_line)
 
-    key_line = key_line.sub("#{@@key_row_char} ","").strip.sub(" ","   ")
+    key_line = key_line.sub("#{@@key_row_char} ", '').strip.sub(' ', '   ')
     key = {}
-    parts = key_line.split(" ")
+    parts = key_line.split(' ')
 
     parts.each_index do |i|
+
       part = parts[i]
       next_part = parts[i + 1]
       open  = key_line.index(part)
       close = next_part ? key_line.index(next_part) : 0
       key[part] = [open, close - open - 1]
+
     end
 
-    return key
+    key
 
   end
 
-  def parse_line key, line
+  def parse_line(key, line)
 
-    if key.length == 1 then return line end
+    return line if key.length == 1
 
     value = {}
     key.each do |index, position|
+
       open = position[0]
       length = position[1]
 
-      data = length < 0 ? line[open, line.length - open].to_s.strip : line[open, length].to_s.strip
-      if data != "" then value[index] = data end
+      data = length.negative? ? line[open, line.length - open].to_s.strip : line[open, length].to_s.strip
+      value[index] = data if data != ''
+
     end
 
-    return value
+    value
 
   end
 
-  def append_a new_line
+  def append_a(new_line)
 
-    unless @a.length == 0 then
-      @a.push(@last_a_type ? Object.const_get(@last_a_type.capitalize).new(new_line, @a.length) : new_line)
-    end
+    return if @a.empty?
+
+    @a.push(@last_a_type ? Object.const_get(@last_a_type.capitalize).new(new_line, @a.length) : new_line)
 
   end
 
-  def update_a memory_index, line
+  def update_a(memory_index, line)
 
-    unless @a.length == 0 then
-      @a[memory_index] = @last_a_type ? Object.const_get(@last_a_type.capitalize).new(line, memory_index) : line
-    end
+    return if @a.empty?
+
+    @a[memory_index] = @last_a_type ? Object.const_get(@last_a_type.capitalize).new(line, memory_index) : line
 
   end
 
 end
 
-#
-
+# Memory_Hash is a Nataniev data structure similar to YAML
 class Memory_Hash
 
   include Memory
@@ -341,51 +350,63 @@ class Memory_Hash
 
   attr_accessor :render
 
-  def ext ; return "mh" end
+  def ext
+    'mh'
+  end
 
-  def filter field = "", value = "", type = ""
+  def filter(_, value = '', type = '')
 
-    return Object.const_get(type.to_s.capitalize).new(value.to_s.capitalize,@render[value.to_s.upcase])
+    Object.const_get(type.to_s.capitalize).new(value.to_s.capitalize, @render[value.to_s.upcase])
 
   end
 
-  def to_h type = nil
+  def to_h(type = nil)
 
     h = {}
-    @render.each do |k,v|
-      h[k] = type ? Object.const_get(type.capitalize).new(k,v) : v
+    @render.each do |k, v|
+
+      h[k] = type ? Object.const_get(type.capitalize).new(k, v) : v
+
     end
-    return h
+    h
 
   end
 
   def save
 
     # Add notes
-    content = @notes.join("\n")+"\n\n"
+    content = "#{@notes.join("\n")}\n\n"
 
-    def make_key_val k, v, depth = 0
-        content = ""
-        if v.kind_of?(Array)
-            content += "#{' ' * depth}#{k}\n"
-            v.each do |val|
-                content += "#{' ' * depth + @@indent_amount}#{val}\n"
-            end
-        elsif v.kind_of?(Hash)
-            content += "#{' ' * depth}#{k}\n"
-            v.each do |key, value|
-                content += make_key_val(key, value, depth + @@indent_amount)
-            end
-        elsif k
-            content += "#{' ' * depth}#{k} : #{v}\n"
+    make_key_val = lambda { |k, v, depth = 0|
+
+      content = ''
+      if v.is_a?(Array)
+        content += "#{' ' * depth}#{k}\n"
+        v.each do |val|
+
+          content += "#{' ' * depth + @@indent_amount}#{val}\n"
+
         end
+      elsif v.is_a?(Hash)
+        content += "#{' ' * depth}#{k}\n"
+        v.each do |key, value|
 
-        return content
-    end
+          content += make_key_val.call(key, value, depth + @@indent_amount)
+
+        end
+      elsif k
+        content += "#{' ' * depth}#{k} : #{v}\n"
+      end
+
+      content
+
+    }
 
     # Create lines
     @render.sort.reverse.each do |key, values|
-      content += make_key_val(key, values)
+
+      content += make_key_val.call(key, values)
+
     end
 
     overwrite(content)
@@ -394,45 +415,50 @@ class Memory_Hash
 
   private
 
-    def make_render file
+  def make_render(file)
 
-        @lines, @notes = make_lines_notes(file)
-        @tree = make_tree
+    @lines, @notes = make_lines_notes(file)
+    @tree = make_tree
 
-        content = {}
-        @tree[-1].each do |id|
-            line = @lines[id].last.strip
-            if @tree[id] then
-                content[line] = make_build(id)
-            elsif line.include? " : "
-                key, val = line.split(" : ")
-                content[key] = val
-            end
-        end
+    content = {}
+    @tree[-1].each do |id|
 
-        return content
+      line = @lines[id].last.strip
+      if @tree[id]
+        content[line] = make_build(id)
+      elsif line.include? ' : '
+        key, val = line.split(' : ')
+        content[key] = val
+      end
 
     end
 
-  def make_lines_notes file
+    content
+
+  end
+
+  def make_lines_notes(file)
 
     notes_a = []
     lines_a = []
 
     number = 0
-    get_file.each do |line|
+    file.each do |line|
+
       depth = line[/\A */].size
       line = line.strip
-      if line == "" then next end
-      if line[0,1] == "~"
+      next if line == ''
+
+      if line[0, 1] == '~'
         notes_a.push(line)
       else
-        lines_a.push([number,depth,line])
+        lines_a.push([number, depth, line])
         number += 1
       end
+
     end
 
-    return lines_a, notes_a
+    [lines_a, notes_a]
 
   end
 
@@ -446,12 +472,12 @@ class Memory_Hash
       l = @lines[i]
       line_number = l[0]
       depth = l[1]
-      if depth == 0 then tree_h[-1].push(line_number) end
+      tree_h[-1].push(line_number) if depth.zero?
       p = line_number
       while p > -1
         pl = @lines[p]
         if pl[1] < depth
-          if !tree_h[pl[0]] then tree_h[pl[0]] = [] end
+          tree_h[pl[0]] = [] unless tree_h[pl[0]]
           tree_h[pl[0]].push(line_number)
           break
         end
@@ -460,36 +486,35 @@ class Memory_Hash
       i += 1
     end
 
-    return tree_h
+    tree_h
 
   end
 
   # TODO: Cleanup
-  def make_build id
+  def make_build(id)
 
-    if !@tree[id] then return end
-    parent = @lines[id].last.strip
+    return unless @tree[id]
 
     t = {}
 
-    @tree[id].each do |id|
-      child = @lines[id].last.strip
-      value = make_build(id)
-      if value != nil
-        if !t.kind_of?(Hash) then t = {} end
+    @tree[id].each do |t_id|
+
+      child = @lines[t_id].last.strip
+      value = make_build(t_id)
+      if !value.nil?
+        t = {} unless t.is_a?(Hash)
         t[child] = value
+      elsif child.include?(' : ') && t.is_a?(Hash)
+        t = {} unless t.is_a?(Hash)
+        t[child.split(' : ').first.strip] = child.split(' : ').last.strip
       else
-        if child.include?(" : ") && t.kind_of?(Hash)
-          if !t.kind_of?(Hash) then t = {} end
-          t[child.split(" : ").first.strip] = child.split(" : ").last.strip
-        else
-          if t.kind_of?(Hash) then t = [] end
-          t.push(child)
-        end
+        t = [] if t.is_a?(Hash)
+        t.push(child)
       end
+
     end
 
-    return t
+    t
 
   end
 
