a0c8e078e11118d3620dbf2282a0f8f3260829ed
Jake Franklin <jacob.d.franklin@gmail.com>
2025-05-27 18:03:21 -0700

    format nataniev.server.rb

--- nataniev.server.rb
+++ nataniev.server.rb
@@ -1,36 +1,42 @@
 #!/bin/env ruby
-# encoding: utf-8
-
 require 'sinatra'
 require 'json'
 
-require_relative "system/nataniev.rb"
+require_relative 'system/nataniev'
 
-set :port, (ARGV.first == "lobby" ? 8668 : 8888)
+set :port, (ARGV.first == 'lobby' ? 8668 : 8888)
 
 configure do
+
   set :bind, '0.0.0.0'
   enable :cross_origin
+
 end
+
+enable :sessions
 
 $nataniev = Nataniev.new
 
 get '/' do
 
-  headers( "Access-Control-Allow-Origin" => "*" )
+  headers('Access-Control-Allow-Origin' => '*')
 
-  puts "================"
+  puts '================'
   puts "request.base_url: #{request.base_url}"
-  puts "================"
+  puts '================'
 
-  v = ARGV.first ? ARGV.first : "ghost"
-  if request.base_url.include? "xxiivv" then v = "landing" end
-  if request.base_url.include? "wiki" then v = "oscean" end
-  if request.base_url.include? "grimgrains" then v = "grimgrains" end
-  if request.base_url.include? "100r" then v = "hundredrabbits" end
-  if request.base_url.include? "paradise" then v = "paradise" end
-  if request.base_url.include? "rotonde" then v = "rotonde" end
-  if request.base_url.include? ":maeve" then v = "maeve" end
+  puts 'Session ================'
+  puts session.inspect
+  puts '================'
+
+  v = ARGV.first || 'ghost'
+  v = 'landing' if request.base_url.include? 'xxiivv'
+  v = 'oscean' if request.base_url.include? 'wiki'
+  v = 'grimgrains' if request.base_url.include? 'grimgrains'
+  v = 'hundredrabbits' if request.base_url.include? '100r'
+  v = 'paradise' if request.base_url.include? 'paradise'
+  v = 'rotonde' if request.base_url.include? 'rotonde'
+  v = 'maeve' if request.base_url.include? ':maeve'
   a = $nataniev.answer("#{v} serve home")
   "#{a}"
 
@@ -39,30 +45,34 @@ end
 get '/:task' do
 
   # TODO: handle favicon
-  if params[:task].include? "favicon.ico" then return end
+  return if params[:task].include? 'favicon.ico'
 
-  headers( "Access-Control-Allow-Origin" => "*" )
+  puts 'Session ================'
+  puts session.inspect
+  puts '================'
 
-  puts "================"
+  headers('Access-Control-Allow-Origin' => '*')
+
+  puts '================'
   puts "GET request.base_url: #{request.base_url}"
   puts "GET request params: #{params}"
-  puts "================"
+  puts '================'
 
-  v = ARGV.first ? ARGV.first : "ghost"
-  if request.base_url.include? "xxiivv" then v = "landing" end
-  if request.base_url.include? "wiki" then v = "oscean" end
-  if request.base_url.include? "grimgrains" then v = "grimgrains" end
-  if request.base_url.include? "100r" then v = "hundredrabbits" end
-  if request.base_url.include? "paradise" then v = "paradise" end
-  if request.base_url.include? "rotonde" then v = "rotonde" end
-  if params[:task].include? ":maeve" then v = "maeve" end
+  v = ARGV.first || 'ghost'
+  v = 'landing' if request.base_url.include? 'xxiivv'
+  v = 'oscean' if request.base_url.include? 'wiki'
+  v = 'grimgrains' if request.base_url.include? 'grimgrains'
+  v = 'hundredrabbits' if request.base_url.include? '100r'
+  v = 'paradise' if request.base_url.include? 'paradise'
+  v = 'rotonde' if request.base_url.include? 'rotonde'
+  v = 'maeve' if params[:task].include? ':maeve'
 
   # Handle the `q` query parameter, if present
-  unless params['q'].nil? then
-    action_params = params['q']
-  else
-    action_params = params[:task]
-  end
+  action_params = if params['q'].nil?
+                    params[:task]
+                  else
+                    params['q']
+                  end
 
   $nataniev.answer("#{v} serve #{action_params}").to_s
 
@@ -70,25 +80,23 @@ end
 
 post '/' do
 
-  headers( "Access-Control-Allow-Origin" => "*" )
+  headers('Access-Control-Allow-Origin' => '*')
 
-  puts "================"
+  puts '================'
   puts "POST request.base_url: #{request.base_url}"
   puts "POST request params: #{params}"
-  puts "================"
+  puts '================'
 
-  v = ARGV.first ? ARGV.first : "ghost"
-  if request.base_url.include? "xxiivv" then v = "landing" end
-  if request.base_url.include? "wiki" then v = "oscean" end
-  if request.base_url.include? "grimgrains" then v = "grimgrains" end
-  if request.base_url.include? "100r" then v = "hundredrabbits" end
-  if request.base_url.include? "paradise" then v = "paradise" end
-  if request.base_url.include? "rotonde" then v = "rotonde" end
+  v = ARGV.first || 'ghost'
+  v = 'landing' if request.base_url.include? 'xxiivv'
+  v = 'oscean' if request.base_url.include? 'wiki'
+  v = 'grimgrains' if request.base_url.include? 'grimgrains'
+  v = 'hundredrabbits' if request.base_url.include? '100r'
+  v = 'paradise' if request.base_url.include? 'paradise'
+  v = 'rotonde' if request.base_url.include? 'rotonde'
 
   # Handle the `q` query parameter, if present
-  unless params['q'].nil? then
-    action_params = params['q']
-  end
+  action_params = params['q'] unless params['q'].nil?
 
   a = $nataniev.answer("#{v} serve #{action_params}")
   "#{a}"
@@ -97,11 +105,11 @@ end
 
 post '/ide.save' do
 
-  path = params["file_path"]
-  code = params["file_content"]
+  path = params['file_path']
+  code = params['file_content']
 
   # Create temp file
-  out_file = File.new("#{path}.tmp", "w")
+  out_file = File.new("#{path}.tmp", 'w')
   out_file.puts(code)
   out_file.close
 
@@ -112,9 +120,9 @@ end
 
 post '/ide.load' do
 
-  path = params["file_path"]
+  path = params['file_path']
 
-  "#{File.read(path, :encoding => 'UTF-8')}"
+  "#{File.read(path, encoding: 'UTF-8')}"
 
 end
 
@@ -123,9 +131,11 @@ post '/ide.tree' do
   a = []
 
   Dir['**/*'].each do |file|
-    ext = file.split(".").last
+
+    ext = file.split('.').last
     # if !["ma","mh","rb","js","css","html"].include?(ext) then next end
     a.push(file)
+
   end
 
   return a.to_json
@@ -134,11 +144,11 @@ end
 
 post '/diary.load' do
 
-  date = params["date"]
+  date = params['date']
 
   h = {}
-  h[:oscean] = $nataniev.summon(:oscean).new.act(:query,"diary")
-  h[:grimgrains] = $nataniev.summon(:grimgrains).new.act(:query,"diary")
+  h[:oscean] = $nataniev.summon(:oscean).new.act(:query, 'diary')
+  h[:grimgrains] = $nataniev.summon(:grimgrains).new.act(:query, 'diary')
 
   return h.to_json
 
@@ -147,28 +157,33 @@ end
 post '/dict.load' do
 
   h = {}
-  Memory_Array.new("dict.russian", $nataniev.path).to_a.each do |word|
-    if !h[word["ENGLISH"]] then h[word["ENGLISH"]] = {} end
-    h[word["ENGLISH"]][:russian] = word["RUSSIAN"]
+  Memory_Array.new('dict.russian', $nataniev.path).to_a.each do |word|
+
+    h[word['ENGLISH']] = {} unless h[word['ENGLISH']]
+    h[word['ENGLISH']][:russian] = word['RUSSIAN']
+
   end
-  Memory_Array.new("dict.lietal", $nataniev.path).to_a.each do |word|
-    if !h[word["ENGLISH"]] then h[word["ENGLISH"]] = {} end
-    h[word["ENGLISH"]][:lietal] = word["LIETAL"]
+  Memory_Array.new('dict.lietal', $nataniev.path).to_a.each do |word|
+
+    h[word['ENGLISH']] = {} unless h[word['ENGLISH']]
+    h[word['ENGLISH']][:lietal] = word['LIETAL']
+
   end
-  Memory_Array.new("dict.traumae", $nataniev.path).to_a.each do |word|
-    if !h[word["ENGLISH"]] then h[word["ENGLISH"]] = {} end
-    h[word["ENGLISH"]][:traumae] = word["TRAUMAE"]
+  Memory_Array.new('dict.traumae', $nataniev.path).to_a.each do |word|
+
+    h[word['ENGLISH']] = {} unless h[word['ENGLISH']]
+    h[word['ENGLISH']][:traumae] = word['TRAUMAE']
+
   end
   return h.to_json
 
 end
 
-
 post '/twitter.feed' do
 
-  account = params["account"]
+  account = params['account']
 
-  require_relative "vessel/vessel.twitter.rb"
-  return $nataniev.summon(:twitter).new.act(:feed,account)
+  require_relative 'vessel/vessel.twitter'
+  return $nataniev.summon(:twitter).new.act(:feed, account)
 
 end
