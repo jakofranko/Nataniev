a1358b0e0f8c3239569c2a411f58415ad8697fde
Jake Franklin <jacob.d.franklin@gmail.com>
2025-06-11 18:30:46 -0700

    Format the Serve action

--- action/action.serve.rb
+++ action/action.serve.rb
@@ -1,32 +1,32 @@
 #!/bin/env ruby
-# encoding: utf-8
 
+# For use with the Nataniev server
 class ActionServe
 
   include Action
 
-  def initialize q = nil
+  def initialize(q = nil)
 
     super
 
-    @name = "Serve"
+    @name = 'Serve'
     @docs = "Serve host's corpse's html."
     @corpse = CorpseBase.new
 
   end
 
-  def act params = nil
+  def act(params = nil)
 
-    @host.corpse.metas = ""
-    @host.corpse.scripts = ""
-    @host.corpse.footer_scripts = ""
-    @host.corpse.links = ""
-    @host.corpse.footers = ""
+    @host.corpse.metas = ''
+    @host.corpse.scripts = ''
+    @host.corpse.footer_scripts = ''
+    @host.corpse.links = ''
+    @host.corpse.footers = ''
 
     @host.corpse.build
     @host.corpse.query(params)
 
-    return @host.corpse.to_html
+    @host.corpse.to_html
 
   end
 
